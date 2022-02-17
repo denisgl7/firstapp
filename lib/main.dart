72c22757dc66bdd672cb96a9e087fd07399d2dd6
denisgladky <de.gladkiy@gmail.com>
2022-02-17 15:16:58 +0300

    Добавили виджет

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-    appBar: AppBar(
-      title: Text(
-        "Привет 4 курс",
-        style: TextStyle(fontSize: 32),
-      ),
-    ),
-    body: Container(
-      decoration: BoxDecoration(
-          color: Colors.green,
-          // border: Border.all(width: 0),
-          borderRadius: BorderRadius.all(Radius.circular(30))),
-      width: 300,
-      height: 400,
-      // padding: EdgeInsets.only(left: 30, top: 50),
-      // margin: EdgeInsets.only(left: 20, top: 70),
-      alignment: Alignment.center,
-    ),
-  )));
-}
+          appBar: AppBar(
+            title: Text(
+              "Привет 4 курс",
+              style: TextStyle(fontSize: 32),
+            ),
+          ),
+          body: Container(
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.adjust, size: 50, color: Colors.orange,),
+                Icon(Icons.adjust, size: 50, color: Colors.orange,),
+                Icon(Icons.adjust, size: 50, color: Colors.orange,)
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              // border: Border.all(width: 0),
+              // borderRadius: BorderRadius.all(Radius.circular(30))),,
+            )
+          ))));
+  }
