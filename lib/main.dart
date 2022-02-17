8b2c65b75c8ca1fe0f02f026925317d98af71c78
denisgladky <de.gladkiy@gmail.com>
2022-02-17 15:54:44 +0300

    Добавили виджет

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,50 @@ void main() {
   runApp(MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: Text(
+            title: const Text(
               "Привет 4 курс",
               style: TextStyle(fontSize: 32),
             ),
           ),
-          body: Container(
-            child: Row(
-              children: <Widget>[
-                Icon(Icons.adjust, size: 50, color: Colors.orange,),
-                Icon(Icons.adjust, size: 50, color: Colors.orange,),
-                Icon(Icons.adjust, size: 50, color: Colors.orange,)
-              ],
-            ),
-            decoration: BoxDecoration(
-              color: Colors.green,
-              // border: Border.all(width: 0),
-              // borderRadius: BorderRadius.all(Radius.circular(30))),,
-            )
+          body: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 200,
+                  height: 300,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(color: Colors.red),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Stack(children: const [
+                      Icon(
+                        Icons.air,
+                        color: Colors.blue,
+                        size: 40,
+                      ),
+                      Icon(
+                        Icons.add,
+                        color: Colors.grey,
+                        size: 50,
+                      )
+                    ],), Stack(children: const [
+                      Icon(
+                        Icons.air,
+                        color: Colors.blue,
+                        size: 40,
+                      ),
+                      Icon(
+                        Icons.add,
+                        color: Colors.grey,
+                        size: 50,
+                      )
+                    ],)],
+                  )),
+              Icon(
+                Icons.airplay,
+                color: Colors.white,
+                size: 200,
+              ),
+            ],
           ))));
-  }
+}
