51154a016e6ef980f3f21e4b297d842715361e80
denisgladky <de.gladkiy@gmail.com>
2022-02-17 15:11:39 +0300

    Добавили виджет

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,16 @@ void main() {
         style: TextStyle(fontSize: 32),
       ),
     ),
-    body: Align(
-      alignment: Alignment(0, 0),
-      child:  FloatingActionButton(
-        tooltip: 'Increment',
-        onPressed: () {  },
-        child: const Icon(Icons.access_alarm),
-      ),
+    body: Container(
+      decoration: BoxDecoration(
+          color: Colors.green,
+          // border: Border.all(width: 0),
+          borderRadius: BorderRadius.all(Radius.circular(30))),
+      width: 300,
+      height: 400,
+      // padding: EdgeInsets.only(left: 30, top: 50),
+      // margin: EdgeInsets.only(left: 20, top: 70),
+      alignment: Alignment.center,
     ),
   )));
 }
